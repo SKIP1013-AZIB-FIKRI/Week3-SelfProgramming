9656bf6f074b25d162301b54bb9634d550252ec7
azibfikri <92501943+azibfikri@users.noreply.github.com>
2021-10-26 12:57:52 +0800

    Create Facts.java

---
+++ Facts.java
@@ -0,0 +1,17 @@
+package selfProgramming;
+
+public class Facts {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We present the following facts for your "
+				            + "extracurricular edification :");
+		System.out.println ();
+		
+		System.out.println ("Letters in the Hawaiian alphabet : 12 \n"
+				            + "Dialing code for Antarctica : " + 672 + "\n"  
+				            + "Year in which Leonardo da Vinci invented " + "the parachute: " + 1515 
+				            + "\n" + "Speed of ketchup: " + 40 + " km per year");
+	}
+
+}
