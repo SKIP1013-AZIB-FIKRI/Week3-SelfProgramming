8caf14ccb2932bb9d9e7baf0e33d3b5452b7a8df
azibfikri <92501943+azibfikri@users.noreply.github.com>
2021-11-01 13:09:56 +0800

    Create Promotion.java

---
+++ Promotion.java
@@ -0,0 +1,18 @@
+package selfProgramming;
+
+public class Promotion {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int count = 12; 
+		double sum = 490.27;
+		double result =  sum / count; /* Data type for count is integer
+		 							                 so because of count 
+		 							                 will be divide with sum which is double 
+		                               so count data type will be change to float 
+		                               this is call promotion*/
+		
+		System.out.print (result);
+	}
+
+}
